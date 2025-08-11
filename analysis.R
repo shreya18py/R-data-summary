c9f8d2b7cf5f75a95156831e93db11946f6b372c
shreya18py <shreyasaksham59@gmail.com>
2025-08-11 23:47:17 +0530

    Add files via upload

---
+++ analysis.R
@@ -0,0 +1,9 @@
+# R script to calculate mean, median, and standard deviation
+data <- c(10, 20, 30, 40, 50)
+mean_value <- mean(data)
+median_value <- median(data)
+sd_value <- sd(data)
+
+print(paste("Mean:", mean_value))
+print(paste("Median:", median_value))
+print(paste("Standard Deviation:", sd_value))
